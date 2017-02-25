099e929af5f2ecadd2bc08722e7f459c65eb5037
Shreyas Ananthan <shreyas@umd.edu>
2017-02-25 18:06:31 -0500

    Add license info on all files

--- src/preprocessing/NDTW2D.h
+++ src/preprocessing/NDTW2D.h
@@ -1,3 +1,18 @@
+//  Copyright 2016 National Renewable Energy Laboratory
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 #ifndef NDTW2D_H
 #define NDTW2D_H
 
@@ -10,6 +25,7 @@ namespace nalu {
  *
  * Calculates a new field NDTW containing the wall distance for 2-D airfoil-like
  * applications used in RANS wall models.
+ *
  */
 class NDTW2D: public PreProcessingTask
 {
