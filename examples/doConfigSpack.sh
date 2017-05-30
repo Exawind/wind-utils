a1e3b1dacd3e105ede06a0266180e980cfdee0ed
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2017-05-30 15:43:23 -0600

    Fixing wrftonalu compiling to use Spack netcdf on Peregrine.
    The example config script uses the shared spack installation on
    Peregrine.

--- examples/doConfigSpack.sh
+++ examples/doConfigSpack.sh
@@ -1,20 +1,38 @@
+#!/bin/bash
 # Example script to configure NaluWindUtils using Spack
 
 # Set the Spack compiler flavor
 COMPILER=gcc
 
+# Use shared Spack on peregine (or change to your Spack's location)
+SPACK_ROOT=/projects/windFlowModeling/ExaWind/NaluSharedInstallation/spack
+SPACK=${SPACK_ROOT}/bin/spack
+
 # Point to appropriate spack installed libraries or local installations
-trilinos_install_dir=$(spack location -i nalu-trilinos %${COMPILER})
-netcdf_install_dir=$(spack location -i netcdf-fortran %${COMPILER})
-yaml_install_dir=$(spack location -i yaml-cpp %${COMPILER})
+trilinos_install_dir=$(${SPACK} location -i nalu-trilinos %${COMPILER})
+yaml_install_dir=$(${SPACK} location -i yaml-cpp %${COMPILER})
+netcdf_install_dir=$(${SPACK} location -i netcdf %${COMPILER})
+netcdffortran_install_dir=$(${SPACK} location -i netcdf-fortran@4.4.3 %${COMPILER})
+
+# Load necessary modules created by spack
+module use ${SPACK_ROOT}/share/spack/modules/$(${SPACK} arch)
+module load $(${SPACK} module find binutils %${COMPILER})
+module load $(${SPACK} module find cmake %${COMPILER})
+module load $(${SPACK} module find openmpi %${COMPILER})
+
+# Clean before cmake configure
+set +e
+rm -rf CMakeFiles
+rm -f CMakeCache.txt
+set -e
 
 EXTRA_ARGS=$@
 
 cmake \
   -DTrilinos_DIR:PATH=$trilinos_install_dir \
-  -DNETCDF_INCLUDE_DIR:PATH=$netcdf_install_dir/include \
-  -DNETCDF_LIBRARY:PATH=$netcdf_install_dir/lib/libnetcdff.a \
   -DYAML_DIR:PATH=$yaml_install_dir \
+  -DNETCDF_DIR:PATH=$netcdf_install_dir \
+  -DNETCDF_F77_ROOT:PATH=$netcdffortran_install_dir \
   -DCMAKE_BUILD_TYPE=Debug \
 $EXTRA_ARGS \
 ../src
